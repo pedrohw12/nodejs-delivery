1679657bacf9eb38d588e31ebdccefbf0f750f14
Pedro Wanderley <wanderley.pedro@outlook.com>
2022-09-18 12:53:52 -0300

    feat: deliveryman authentication

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
+import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
@@ -7,10 +8,13 @@ const routes = Router();
 
 const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
+const authenticateDeliverymanController =
+  new AuthenticateDeliverymanController();
 const createDeliverymanController = new CreateDeliverymanController();
 
 routes.post("/clients/", createClientController.handle);
-routes.post("/auth", authenticateClientController.handle);
+routes.post("/client/auth", authenticateClientController.handle);
+routes.post("/deliveryman/auth", authenticateDeliverymanController.handle);
 routes.post("/deliveryman", createDeliverymanController.handle);
 
 export { routes };
